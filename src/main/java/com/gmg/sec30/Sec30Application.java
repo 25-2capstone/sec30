dc3385abd1e261374aa2766f6be5f1de5b8ee427
lovesome-gy <sgy0625@naver.com>
2025-11-09 13:37:08 +0900

    [PM] Initial commit: Spring project setup

---
+++ src/main/java/com/gmg/sec30/Sec30Application.java
@@ -0,0 +1,13 @@
+package com.gmg.sec30;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sec30Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sec30Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gmg/sec30/Sec30ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmg.sec30;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sec30ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
